23cf03a795bcdcaf587d005e17102ad4e4f3d599
Alon Lanyado <alonyado@gmail.com>
2025-10-14 12:29:36 +0300

    Imprved build script

--- Docker/AlgoMarker_Server/create_image.sh
+++ Docker/AlgoMarker_Server/create_image.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 set -e
+CURR_DIR=${0%/*}
 # Change to the directory of this script
-cd ${0%/*}
+cd ${CURR_DIR}
 
-AM_CONFIG_PATH=${1-/app/LGI-Flag-ButWhy-3.1.2-Scorer/LGI-ColonFlag-3.1.amconfig}
+AM_CONFIG_PATH=${1}
 PORT=${2-1234}
 
+if [ -z ${AM_CONFIG_PATH} ]; then
+    echo "Usage: $0 <AM_CONFIG_PATH> [PORT]"
+    echo "  AM_CONFIG_PATH: Path to AlgoMarker config path (e.g., /path/to/.amconfig)"
+    echo "  PORT: (Optional) Port number for the server (default: 1234)"
+    exit 1
+fi
+
+AM_CONFIG_PATH=$(realpath ${AM_CONFIG_PATH})
+AM_CONFIG_PATH_DOCKER=$(realpath -s --relative-to="${CURR_DIR}/data" "${AM_CONFIG_PATH}")
+
 # Make sure paths exists, under "data" folder:
-if [ ! -f "data$AM_CONFIG_PATH" ]; then
+if [ ! -f "${AM_CONFIG_PATH}" ]; then
     echo "Error: AM_CONFIG_PATH '$AM_CONFIG_PATH' does not exist."
     exit 1
 fi
 AM_DIR=$(dirname "$AM_CONFIG_PATH")
-if [ ! -f "data$AM_DIR/lib/libdyn_AlgoMarker.so" ]; then
-    echo "Error: data$AM_CONFIG_PATH/lib/libdyn_AlgoMarker.so does not exist. Please put the library there."
+if [ ! -f "${AM_DIR}/lib/libdyn_AlgoMarker.so" ]; then
+    echo "Error: $AM_CONFIG_PATH/lib/libdyn_AlgoMarker.so does not exist. Please put the library there."
     exit 1
 fi
 
 # Generate a Dockerfile from the template
-sed -e "s|\${AM_CONFIG_PATH}|${AM_CONFIG_PATH}|g" -e "s|\${PORT}|${PORT}|g" Dockerfile.template > Dockerfile
+sed -e "s|\${AM_CONFIG_PATH}|/${AM_CONFIG_PATH_DOCKER}|g" -e "s|\${PORT}|${PORT}|g" Dockerfile.template > Dockerfile
 
 # Run build
 podman build -t lgiflag_app --no-cache .
